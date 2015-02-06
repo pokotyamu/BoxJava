e135346716ad982df37ebc2afcc2c1fe7c121345
pokotyamu <pokotyamu@gmail.com>
2015-02-06 17:43:40 +0900

    add GraphData and seriesparser

--- src/sqlbox/Main.java
+++ src/sqlbox/Main.java
@@ -7,6 +7,8 @@ package sqlbox;
 
 
 import Functionbox.Max;
+import Graphbox.GraphDataSet;
+import Graphbox.SeriesParser;
 import PSPData.DataSet;
 import box.AbstractBox;
 
@@ -23,8 +25,18 @@ public class Main {
         String whereString = " WHERE CLASS_ID=201301 AND SUBMITION_ID=1 ORDER BY ST_ID ASC,PROJECTID ASC, SUBMITION_ID ASC";
         AbstractBox actmin = new ACTMIN("PROJECTID", "ACTMIN");
         AbstractBox max = new Max();
-        max.actionBox(actmin.actionBox(whereString)).debugPrint();
-       
+        SeriesParser sp = new SeriesParser();
+        DataSet minDataSet = actmin.actionBox(whereString);
+        minDataSet.debugPrint();
+        
+        System.out.println("===");
+        GraphDataSet parse = sp.parse(minDataSet);
+        parse.debugPrint();
+        System.out.println("===");
+        DataSet maxdDataSet = max.actionBox(minDataSet);
+        maxdDataSet.debugPrint();
+        sp.parse(maxdDataSet).debugPrint();
+        
 
 
     }
